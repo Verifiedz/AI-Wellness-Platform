9239b947eea801839366b19f478f848a0af62e7c
Verifiedz <saadwasim1921@gmail.com>
2026-01-01 21:58:16 -0500

    ChatService-stage2-v2

--- chat-service/ChatService/Services/chatService.cs
+++ chat-service/ChatService/Services/chatService.cs
@@ -1,5 +1,28 @@
-﻿namespace ChatService.Services;
-
-public class chatService
+﻿namespace ChatService.Services; 
+using ChatService.Interfaces;
+public class chatService: IChatService
 {
+  
+ private readonly IChatAPIGatwayInterface _chatApiGatewayInterface; 
+ 
+ private readonly IChatWrapperClientInterface _chatWrapperClientInterface;
+
+ public chatService(IChatAPIGatwayInterface chatAPIGatway,IChatWrapperClientInterface chatWrapperClientInterface){
+
+
+   _chatApiGatewayInterface = chatAPIGatway; 
+   _chatWrapperClientInterface = chatWrapperClientInterface; 
+ }
+
+
+
+ public void ExecuteChatService(){
+
+
+  }
+
+  public void ModifyChats(){
+
+  }
+ 
 }
